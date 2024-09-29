6ead5cab483f371a806fcb0dec3d1e6fd8dac8ca
abhitiku2003 <abhitiku2003@gmail.com>
2024-09-29 14:08:37 +0000

    fixes

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -3,11 +3,4 @@ const nextConfig = {transpilePackages: ["geist"],serverRuntimeConfig: {
     // This will be available on both server and client sides
     functionMaxDuration: 60 // in seconds
   },};
-  if (process.env.VERCEL) {
-    nextConfig.functions = {
-      'api/content/getTag': {
-        maxDuration: 60
-      }
-    };
-  }
 export default nextConfig;
